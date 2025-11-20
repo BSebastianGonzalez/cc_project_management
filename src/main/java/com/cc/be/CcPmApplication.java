113c7f70b6b5d643f786474ae1621ab33504353a
BSebastianGonzalez <bsgonzalez14a@gmail.com>
2025-11-19 22:11:43 -0500

    first commit

---
+++ src/main/java/com/cc/be/CcPmApplication.java
@@ -0,0 +1,13 @@
+package com.cc.be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcPmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcPmApplication.class, args);
+	}
+
+}
